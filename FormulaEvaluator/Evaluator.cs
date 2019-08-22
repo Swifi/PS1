df424ea893d2beca6469daa63781c43e7aba7fda
Swifi <nick.hayes333@gmail.com>
2019-08-22 13:09:29 -0600

    Created Project

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/**
+ * Created by Nick Hayes. Thisclass evaluates arithmetic
+ * expressions using standard infix notation
+ **/
+namespace FormulaEvaluator
+{
+    public static class Evaluator
+    {
+    }
+}
